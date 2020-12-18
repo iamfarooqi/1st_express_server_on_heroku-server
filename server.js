a470dc2377fa11d9aa77d0adfb2b7694e9d8abc0
farooqi <attaurrahmanfarooqi9@gmal.com>
2020-12-18 17:21:22 +0500

    some changes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 var express = require("express");
 
 var server = express();
@@ -6,6 +7,15 @@ var PORT = process.env.PORT || 5000
 server.get("/", (req, res, next) => {
     console.log("some one get menu");
     res.send("menu: what do you want? food or water");
+})
+server.get("/water", (req, res, next) => {
+    console.log("some one is asking water");
+    res.send("here is water");
+})
+
+server.get("/food", (req, res, next) => {
+    console.log("some one is asking food");
+    res.send("here is food");
 })
 
 server.listen(PORT, () => {
