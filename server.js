12e19c45adf0d37ec959465a06152f2c076486ee
farooqi <attaurrahmanfarooqi9@gmal.com>
2020-12-18 16:35:27 +0500

    Minor.

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require("express");
 
 var server = express();
+var PORT = process.env.PORT || 5000
 
 server.get("/", (req, res, next) => {
     console.log("some one get menu");
     res.send("menu: what do you want? food or water");
 })
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log("server is running on port 3000");
 })
